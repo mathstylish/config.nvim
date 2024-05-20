4e7e8960569f42c5f6d17c85797f31ee827fa37e
Matheus Enrique <matheus.hpp135@gmail.com>
2024-05-20 14:29:55 -0300

    feat: 🎸 ui improvements

--- init.lua
+++ init.lua
@@ -5,6 +5,14 @@ require "user.core.autocmds"
 require "user.launch"
 
 spec "user.core.treesitter"
+
 spec "user.ui.theme"
+spec "user.ui.lualine"
+spec "user.ui.indentline"
+spec "user.ui.indentscope"
+
+spec "user.extras.ui.nui"
+spec "user.extras.ui.noice"
+spec "user.extras.ui.dressing"
 
 require "user.lazy"
