2a79bf7f955392ab8f2bb0f048e460edc26e2761
Matheus Enrique <matheus.hpp135@gmail.com>
2024-06-03 17:02:11 -0300

    feat: 🎸 carbonfox colorscheme

--- lua/user/core/theme.lua
+++ lua/user/core/theme.lua
@@ -1,15 +1,11 @@
 local M = {
-  "Everblush/nvim",
-  name = "everblush",
+  "EdenEast/nightfox.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  require("everblush").setup {
-    transparent_background = true
-  }
-  vim.cmd.colorscheme "everblush"
+  vim.cmd.colorscheme "carbonfox"
 end
 
 return M
