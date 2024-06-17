b9d3f775e46547c2118dcf23ad341658bb5b9ded
Matheus Enrique <matheus.hpp135@gmail.com>
2024-06-17 10:57:58 -0300

    refactor: 💡 change surround mappings

--- lua/user/core/surround.lua
+++ lua/user/core/surround.lua
@@ -1,8 +1,34 @@
 local M = {
   "kylechui/nvim-surround",
-  version = "*",
   event = "VeryLazy",
-  opts = {},
 }
+
+function M.config()
+  require("nvim-surround").setup {
+    keymaps = { -- vim-surround style keymaps
+      insert = "<C-g>s",
+      insert_line = "<C-g>S",
+      normal = "s",
+      visual = "s",
+      delete = "ds",
+      change = "cs",
+      normal_cur = "ss",
+      normal_line = "S",
+      normal_cur_line = "SS",
+      visual_line = "gS",
+    },
+  }
+  vim.cmd [[ nmap <leader>' _saw' ]]
+  vim.cmd [[ nmap <leader>" _saw" ]]
+  vim.cmd [[ nmap <leader>{ _saw{ ]]
+  vim.cmd [[ nmap <leader>[ _saw[ ]]
+  vim.cmd [[ nmap <leader>( _saw( ]]
+
+  vim.cmd [[ vmap <leader>' s' ]]
+  vim.cmd [[ vmap <leader>" s" ]]
+  vim.cmd [[ vmap <leader>{ s{ ]]
+  vim.cmd [[ vmap <leader>[ s[ ]]
+  vim.cmd [[ vmap <leader>( s( ]]
+end
 
 return M
