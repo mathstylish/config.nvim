7c706901886360040e223c5e221bbf2461c69e01
Matheus Henrique <matheus.hpp135@gmail.com>
2024-06-17 21:13:11 -0300

    feat: 🎸 terminal basics

--- lua/user/extras/toggleterm.lua
+++ lua/user/extras/toggleterm.lua
@@ -7,13 +7,8 @@ local M = {
 function _G.set_terminal_keymaps()
   local keymap = vim.keymap
   local opts = { buffer = 0 }
-  keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-  keymap.set("t", "jk", [[<C-\><C-n>]], opts)
   keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-  keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-  keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
   keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-  keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
 end
 
 function M.config()
