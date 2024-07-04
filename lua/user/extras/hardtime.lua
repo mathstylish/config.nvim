faef2d3dabff37f19bcaed244833f32144e03631
Matheus Enrique <matheus.hpp135@gmail.com>
2024-07-04 12:53:21 -0300

    feat: 🎸 new hints

--- lua/user/extras/hardtime.lua
+++ lua/user/extras/hardtime.lua
@@ -1,12 +1,19 @@
 local M = {
   "m4xshen/hardtime.nvim",
-  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  event = "VeryLazy",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "rcarriga/nvim-notify" },
 }
 
 function M.config()
+  vim.notify = require "notify"
   require("hardtime").setup {
     disable_mouse = false,
+    hints = {
+      ["cc"] = {
+        message = function()
+          return "Use S instead of cc"
+        end,
+      },
+    },
   }
 end
 
